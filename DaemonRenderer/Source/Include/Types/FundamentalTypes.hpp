b579c350bc9f5b0fe87f472b4422b64fc3f3bde1
Renondedju <21277516+Renondedju@users.noreply.github.com>
2020-03-25 16:29:44 +0100

    Fixed licence (#7)
    The license was sill indicating "2019" and has been changed to "2019-2020".

--- DaemonRenderer/Source/Include/Types/FundamentalTypes.hpp
+++ DaemonRenderer/Source/Include/Types/FundamentalTypes.hpp
@@ -1,7 +1,7 @@
 /**
  *  MIT License
  *
- *  Copyright (c) 2019 Basile Combet, Philippe Yi
+ *  Copyright (c) 2019-2020 Basile Combet, Philippe Yi
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
