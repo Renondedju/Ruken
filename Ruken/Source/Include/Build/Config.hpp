000d4f1cf649e4a6de0903d629138c909949cd5d
Basile Combet <b.combet@student.isartdigital.com>
2020-10-16 17:57:56 +0200

    Cleaned up config

--- Ruken/Source/Include/Build/Config.hpp
+++ Ruken/Source/Include/Build/Config.hpp
@@ -43,16 +43,16 @@
 #define RUKEN_DEBUG   if constexpr(RUKEN_CONFIG_LEVEL == RUKEN_CONFIG_LEVEL_DEBUG)
 #define RUKEN_RELEASE if constexpr(RUKEN_CONFIG_LEVEL == RUKEN_CONFIG_LEVEL_RELEASE)
 
-#if !defined(no_multithread) && defined(__STDCPP_THREADS__)
+// ------------------------------
+//            Threading
+
+#if !defined(no_multithread) && defined(__STDCPP_THREADS__) && !defined(RUKEN_REQUEST_SINGLE_THREADED_BUILD)
     #define RUKEN_MULTITHREAD_ENABLED
     #define RUKEN_MULTITHREAD_STATUS_STR "Enabled"
 #else
     #define RUKEN_MULTITHREAD_DISABLED
     #define RUKEN_MULTITHREAD_STATUS_STR "Disabled"
 #endif
-
-// ------------------------------
-//            Threading
 
 #if defined(RUKEN_CONFIG_DEBUG)
     #define RUKEN_THREADING_ENABLE_THREAD_LABELS
@@ -73,3 +73,14 @@
 // Sets the maximum number of components allowed by the ECS, keep this number
 // as low as possible. Must be a power of 2 with a minimum of 8.
 #define RUKEN_MAX_ECS_COMPONENTS 64
+
+// ------------------------------
+//            Logging
+
+#if !defined(RUKEN_REQUEST_SILENT_BUILD)
+    #define RUKEN_LOGGING_ENABLED
+    #define RUKEN_LOGGING_STATUS_STR "Enabled"
+#else
+    #define RUKEN_LOGGING_DISABLED
+    #define RUKEN_LOGGING_STATUS_STR "Disabled"
+#endif
