56c303c6b8815457378e4dca187331f6f392d540
Basile Combet <21277516+Renondedju@users.noreply.github.com>
2021-09-24 15:25:04 +0200

    Implemented strong type arithmetic operators

---
+++ Ruken/Source/Include/Types/StrongType/Operators/Arithmetic/Decrement.hpp
@@ -0,0 +1,75 @@
+/**
+ *  MIT License
+ *
+ *  Copyright (c) 2019-2020 Basile Combet, Philippe Yi
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+#pragma once
+
+#include "Build/Namespace.hpp"
+#include "Types/FundamentalTypes.hpp"
+#include "Types/StrongType/StrongTypeHelper.hpp"
+
+BEGIN_RUKEN_NAMESPACE
+
+/**
+ * \brief Decrement operator class
+ * 
+ * This class is meant to be used in conjunction with the NamedType class.
+ * This allows for better and quicker operator integrations to named types.
+ * 
+ * \tparam TStrongType Base NamedType
+ *
+ * \see NamedType
+ */
+template <typename TStrongType>
+class Decrement
+{
+    using Type = StrongTypeHelper::UnderlyingType<TStrongType>;
+
+    public:
+
+        #pragma region Operators
+
+        /**
+         * \brief Pre-Decrement operator
+         * \param in_instance Operand instance
+         * \return Reference to the instance
+         */
+        friend constexpr TStrongType& operator--(TStrongType& in_instance) noexcept
+        {
+            return --static_cast<Type&>(in_instance);
+        }
+
+        /**
+         * \brief Post-Decrement operator
+         * \param in_instance Operand instance
+         * \return Value of the new instance
+         */
+        friend constexpr TStrongType operator--(TStrongType& in_instance, RkInt) noexcept
+        {
+            return static_cast<Type&>(in_instance)--;
+        }
+
+        #pragma endregion
+};
+
+END_RUKEN_NAMESPACE
