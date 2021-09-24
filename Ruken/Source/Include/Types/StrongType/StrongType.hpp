cc410e852ba62b11340e2da78d9aedc390b08841
Basile Combet <21277516+Renondedju@users.noreply.github.com>
2021-09-24 15:23:44 +0200

    Implemented strong types

---
+++ Ruken/Source/Include/Types/StrongType/StrongType.hpp
@@ -0,0 +1,90 @@
+/**
+ *  MIT License
+ *
+ *  Copyright (c) 2019-2020 Basile Combet, Philippe Yi
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+#pragma once
+
+#include <type_traits>
+
+#include "Build/Namespace.hpp"
+#include "Types/Concepts/FundamentalType.hpp"
+
+BEGIN_RUKEN_NAMESPACE
+
+/**
+ * \brief Eases the creation of strong types
+ * 
+ * Every method is constexpr and noexcept if the base type is noexcept as well
+ * 
+ * To create a "complex" NamedType, simply inherit from this class and overload all the required operators.
+ * Do note that every operator class like Addition or Modulo, can also be inherited to save you some time.
+ * 
+ * Example :
+ * 
+ * // This class is strongly typed and only has the addition and subtraction operators available
+ * struct Meter : NamedType<float, Meter>, Addition<Meter>, Subtraction<Meter>
+ * { using NamedType::NamedType };
+ * 
+ * \tparam TBase Base type, could be any fundamental type
+ * \tparam TUniquePhantom Phantom type, this should be unique for each type
+ * \see https://foonathan.net/blog/2016/10/19/strong-typedefs.html
+ * \see https://arne-mertz.de/2016/11/stronger-types/
+ */
+template <FundamentalType TBase, typename TUniquePhantom>
+class StrongType
+{
+    #pragma region Members
+
+    TBase m_value {};
+
+    #pragma endregion
+
+    public:
+
+        using UnderlyingType = TBase;
+
+        #pragma region Constructors
+
+        explicit constexpr StrongType(TBase const& in_copy) noexcept;
+        explicit constexpr StrongType(TBase&&      in_move) noexcept;
+
+        explicit constexpr StrongType (StrongType const& in_copy) = default;
+        explicit constexpr StrongType (StrongType&&      in_move) = default;
+                           ~StrongType()                          = default;
+
+        #pragma endregion
+
+        #pragma region Operators
+
+        explicit constexpr operator TBase&      ()       noexcept;
+        explicit constexpr operator TBase const&() const noexcept;
+
+        constexpr StrongType& operator=(StrongType const& in_copy) = default;
+        constexpr StrongType& operator=(StrongType&&      in_move) = default;
+
+        #pragma endregion
+};
+
+#include "Types/StrongType/StrongType.inl"
+
+END_RUKEN_NAMESPACE
---
+++ Ruken/Source/Src/Types/StrongType/StrongType.inl
@@ -0,0 +1,55 @@
+/**
+ *  MIT License
+ *
+ *  Copyright (c) 2019-2020 Basile Combet, Philippe Yi
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+#pragma once
+
+#pragma region Constructor
+
+template <FundamentalType TBase, typename TUniquePhantom>
+constexpr StrongType<TBase, TUniquePhantom>::StrongType(TBase const& in_copy) noexcept:
+    m_value {in_copy}
+{}
+
+template <FundamentalType TBase, typename TUniquePhantom>
+constexpr StrongType<TBase, TUniquePhantom>::StrongType(TBase&& in_move) noexcept:
+    m_value {std::forward<TBase>(in_move)}
+{}
+
+#pragma endregion
+
+#pragma region Operators
+
+template <FundamentalType TBase, typename TUniquePhantom>
+constexpr StrongType<TBase, TUniquePhantom>::operator TBase&() noexcept
+{
+    return m_value;
+}
+
+template <FundamentalType TBase, typename TUniquePhantom>
+constexpr StrongType<TBase, TUniquePhantom>::operator TBase const&() const noexcept
+{
+    return m_value;
+}
+
+#pragma endregion
