c2aa5d9ce366a90fd94e82254ddc3a2def26c38b
Basile Combet <21277516+Renondedju@users.noreply.github.com>
2021-10-20 09:55:19 +0200

    Added CommonTypeFallback meta

---
+++ Ruken/Source/Include/Meta/CommonTypeFallback.hpp
@@ -0,0 +1,54 @@
+/**
+ *  MIT License
+ *
+ *  Copyright (c) 2019-2020 Basile Combet, Philippe Yi
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+#pragma once
+
+#include <type_traits>
+
+#include "Build/Namespace.hpp"
+#include "Meta/HasCommonType.hpp"
+
+BEGIN_RUKEN_NAMESPACE
+
+template <typename TAlwaysVoid, typename TFallbackType, typename... TTypes>
+struct CommonTypeFallbackImpl
+{
+	using Type = TFallbackType;
+};
+
+template <typename TFallbackType, typename... TTypes>
+struct CommonTypeFallbackImpl<std::void_t<std::common_type_t<TTypes...>>, TFallbackType, TTypes...>
+{
+    using Type = std::common_type_t<TTypes...>;
+};
+
+/**
+ * \brief Returns a common type if it exists, otherwise returns TFallbackType
+ * \tparam TFallbackType Fallback or default type
+ * \tparam TTypes Types to test
+ */
+template <typename TFallbackType, typename... TTypes>
+using CommonTypeFallback = CommonTypeFallbackImpl<RkVoid, TFallbackType, TTypes...>::Type;
+
+END_RUKEN_NAMESPACE
