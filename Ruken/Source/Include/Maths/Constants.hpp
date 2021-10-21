263cd47850aa10efb02965a8f136fc6c45bd404d
Basile Combet <21277516+Renondedju@users.noreply.github.com>
2021-10-21 10:31:39 +0200

    Constants are now contained in a Constant struct

--- Ruken/Source/Include/Maths/Constants.hpp
+++ Ruken/Source/Include/Maths/Constants.hpp
@@ -24,32 +24,35 @@
 
 #pragma once
 
-#include <limits>
-
 #include "Build/Namespace.hpp"
 #include "Types/FundamentalTypes.hpp"
 
 BEGIN_RUKEN_NAMESPACE
 
-constexpr RkFloat g_e         = 2.71828182845904523536F; // e
-constexpr RkFloat g_log2_e    = 1.44269504088896340736F; // log2(e)
-constexpr RkFloat g_log10_e   = 0.43429448190325182765F; // log10(e)
-constexpr RkFloat g_ln2       = 0.69314718055994530942F; // ln(2)
-constexpr RkFloat g_ln10      = 2.30258509299404568402F; // ln(10)
-constexpr RkFloat g_pi        = 3.14159265358979323846F; // pi
-constexpr RkFloat g_pi_2      = 1.57079632679489661923F; // pi/2
-constexpr RkFloat g_pi_4      = 0.78539816339744830962F; // pi/4
-constexpr RkFloat g_1_pi      = 0.31830988618379067154F; // 1/pi
-constexpr RkFloat g_2_pi      = 0.63661977236758134308F; // 2/pi
-constexpr RkFloat g_2_sqrt_pi = 1.12837916709551257390F; // 2/sqrt(pi)
-constexpr RkFloat g_sqrt_2    = 1.41421356237309504880F; // sqrt(2)
-constexpr RkFloat g_1_sqrt_2  = 0.70710678118654752440F; // 1/sqrt(2)
+/**
+ * \brief Holds the constants for every passed type 
+ * \tparam TType Type to find the constants for
+ */
+template <typename TType>
+struct Constants
+{};
 
-// Returns the machine epsilon, that is, the difference between 1.0 and the next value representable by the floating-point type T
-constexpr RkFloat  g_float_epsilon  = std::numeric_limits<RkFloat >::epsilon();
-constexpr RkDouble g_double_epsilon = std::numeric_limits<RkDouble>::epsilon();
-
-constexpr RkFloat  g_float_infinity  = std::numeric_limits<RkFloat> ::infinity();
-constexpr RkDouble g_double_infinity = std::numeric_limits<RkDouble>::infinity();
+template <>
+struct Constants<RkFloat>
+{
+	static constexpr RkFloat e          = 2.71828182845904523536F; // e
+    static constexpr RkFloat log2_e     = 1.44269504088896340736F; // log2(e)
+    static constexpr RkFloat log10_e    = 0.43429448190325182765F; // log10(e)
+    static constexpr RkFloat ln2        = 0.69314718055994530942F; // ln(2)
+    static constexpr RkFloat ln10       = 2.30258509299404568402F; // ln(10)
+    static constexpr RkFloat pi         = 3.14159265358979323846F; // pi
+    static constexpr RkFloat pi_2       = 1.57079632679489661923F; // pi/2
+    static constexpr RkFloat pi_4       = 0.78539816339744830962F; // pi/4
+    static constexpr RkFloat _1_pi      = 0.31830988618379067154F; // 1/pi
+    static constexpr RkFloat _2_pi      = 0.63661977236758134308F; // 2/pi
+    static constexpr RkFloat _2_sqrt_pi = 1.12837916709551257390F; // 2/sqrt(pi)
+    static constexpr RkFloat sqrt_2     = 1.41421356237309504880F; // sqrt(2)
+    static constexpr RkFloat _1_sqrt_2  = 0.70710678118654752440F; // 1/sqrt(2)
+};
 
 END_RUKEN_NAMESPACE
