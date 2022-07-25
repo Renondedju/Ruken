58977b3fa9de5814b7d4a906d14ec54b26a2a127
Basile Combet <21277516+Renondedju@users.noreply.github.com>
2022-07-25 15:04:27 +0200

    Cleaned up build folder

--- Ruken/Source/Include/Build/Info.hpp
+++ Ruken/Source/Include/Build/Info.hpp
@@ -1,7 +1,6 @@
-
 #pragma once
 
-#define RUKEN_COPYRIGHT_STR "Copyright (c) 2019-2020 Basile Combet, Philippe Yi"
+#define RUKEN_COPYRIGHT_STR "Copyright (c) 2019-2022 Basile Combet, Philippe Yi"
 #define RUKEN_LICENSE_STR   "MIT License"
 #define RUKEN_PROJECT_NAME  "Ruken"
 #define RUKEN_URL           "https://github.com/Renondedju/Ruken"
--- Ruken/Source/Include/Build/OperatingSystem.hpp
+++ Ruken/Source/Include/Build/OperatingSystem.hpp
@@ -1,4 +1,3 @@
-
 #pragma once
 
 #if defined(_WIN32) || defined(_WIN64)
